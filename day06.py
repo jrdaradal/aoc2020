dd2620365a2480d9526ab06f501f9cd841e66934
Roi Daradal <roidaradal@gmail.com>
2025-04-09 18:31:23 +0800

    v0.6.2 - Day 06.2

--- day06.py
+++ day06.py
@@ -2,6 +2,7 @@
 # John Roy Daradal 
 
 # SolutionA: 6763
+# SolutionB: 3512
 
 from utils import * 
 
@@ -26,11 +27,28 @@ def day06A():
         total += countYes(g)
     print(total)
 
+def day06B():
+    full = True 
+    total = 0 
+    for g in input06(full):
+        total += countAllYes(g)
+    print(total)
+
 def countYes(g: group) -> int:
     s = set()
     for x in g:
         s = s.union(set(x))
     return len(s)
 
+def countAllYes(g: group) -> int:
+    count = {}
+    for x in g:
+        for c in x:
+            count.setdefault(c, 0)
+            count[c] += 1
+    return len([k for k,v in count.items() if v == len(g)])
+
+
 if __name__ == '__main__':
     day06A()
+    day06B()
