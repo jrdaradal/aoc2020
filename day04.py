4f026f69eaa38ffe4577db91d17aac7efd1c5109
Roi Daradal <roidaradal@gmail.com>
2025-04-09 17:29:54 +0800

    v0.4.2 - Day 04.2

--- day04.py
+++ day04.py
@@ -2,7 +2,9 @@
 # John Roy Daradal 
 
 # SolutionA: 254
+# SolutionB: 184
 
+import re
 from utils import * 
 
 passport = dict[str,str]
@@ -28,9 +30,54 @@ def day04A():
             count += 1 
     print(count)
 
+def day04B():
+    full = True 
+    count = 0 
+    for p in input04(full):
+        if isFullyValid(p):
+            count += 1 
+    print(count)
+
 required = ('byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid')
 def isValid(p: passport) -> bool:
     return all(k in p for k in required)
 
+def isFullyValid(p: passport) -> bool:
+    ok = 0 
+    for k, v in p.items():
+        if k == 'byr':
+            x = int(v)
+            if len(v) == 4 and 1920 <= x and x <= 2002:
+                ok += 1
+        elif k == 'iyr':
+            x = int(v)
+            if len(v) == 4 and 2010 <= x and x <= 2020:
+                ok += 1
+        elif k == 'eyr':
+            x = int(v)
+            if len(v) == 4 and 2020 <= x and x <= 2030:
+                ok += 1
+        elif k == 'hgt':
+            if v.endswith('cm') or v.endswith('in'):
+                unit = v[-2:]
+                x = int(v[:-2])
+                if unit == 'cm' and 150 <= x and x <= 193:
+                    ok += 1
+                elif unit == 'in' and 59 <= x and x <= 76:
+                    ok += 1
+        elif k == 'hcl':
+            pattern = r'^#[0-9a-f]{6}$'
+            if re.match(pattern, v) != None:
+                ok += 1
+        elif k == 'ecl':
+            if v in ('amb','blu','brn','gry','grn','hzl','oth'):
+                ok += 1
+        elif k == 'pid':
+            pattern = r'^[0-9]{9}$'
+            if re.match(pattern, v) != None:
+                ok += 1
+    return ok == len(required)
+
 if __name__ == '__main__':
     day04A()
+    day04B()
