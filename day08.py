91cd9b3b0bffb474aaff08911c4560ac55c3eeb2
Roi Daradal <roidaradal@gmail.com>
2025-04-10 21:43:53 +0800

    v0.8.2 - Day 08.2

--- day08.py
+++ day08.py
@@ -2,7 +2,7 @@
 # John Roy Daradal 
 
 # SolutionA: 1744
-
+# SolutionB: 1174
 
 from utils import *
 
@@ -17,21 +17,47 @@ def input08(full: bool) -> list[code]:
 def day08A():
     full = True 
     codes = input08(full)
+    x, _ = runCodes(codes)
+    print(x)
+
+def day08B():
+    full = True 
+    codes = input08(full)
+    for i,(cmd,_) in enumerate(codes):
+        if cmd == 'nop' or cmd == 'jmp':
+            codes2 = flipNopJmp(codes, i)
+            x, stuck = runCodes(codes2)
+            if not stuck:
+                print(x)
+                return
+
+def runCodes(codes: list[code]) -> tuple[int,bool]:
     i, x = 0, 0
-    done: set[int] = set()
+    done: set[int] = set() 
+    numCodes = len(codes)
     while True:
         if i in done:
-            print(x)
-            return 
+            return (x, True)
+        if i >= numCodes:
+            return (x, False)
+        
         done.add(i)
-        cmd, param = codes[i]
+        cmd, y = codes[i]
         if cmd == 'nop':
-            i += 1
-        elif cmd == 'acc':
-            x += param 
             i += 1 
+        elif cmd == 'acc':
+            x += y 
+            i += 1
         elif cmd == 'jmp':
-            i += param
+            i += y 
+
+def flipNopJmp(codes: list[code], idx: int) -> list[code]:
+    codes2 = codes[:]
+    cmd,y = codes2[idx]
+    cmd2 = 'nop' if cmd == 'jmp' else 'jmp'
+    codes2[idx] = (cmd2,y)
+    return codes2
 
 if __name__ == '__main__':
     day08A()
+    day08B()
