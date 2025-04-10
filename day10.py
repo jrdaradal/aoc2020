afd03aa12172f5222a548a8876ed087b5844e961
Roi Daradal <roidaradal@gmail.com>
2025-04-10 23:07:19 +0800

    v0.10.2 - Day 10.2

--- day10.py
+++ day10.py
@@ -2,6 +2,7 @@
 # John Roy Daradal 
 
 # SolutionA: 2590
+# SolutionB: 226775649501184
 
 from utils import *
 
@@ -22,5 +23,19 @@ def day10A():
         diffs[d] += 1 
     print(diffs[1] * diffs[3])
 
+def day10B():
+    full = True 
+    numbers = input10(full)
+    numbers.sort()
+    count = {numbers[-1]: 1}
+    i = len(numbers) - 2 
+    while i >= 0:
+        curr = numbers[i]
+        valid = [x for x in numbers[i+1:i+4] if x-curr <= 3]
+        count[curr] = sum(count[x] for x in valid)
+        i -= 1
+    print(count[numbers[0]])
+
 if __name__ == '__main__':
     day10A()
+    day10B()
