338e287b9e5f13255cc56230bcb0838ed876012a
Roi Daradal <roidaradal@gmail.com>
2025-04-10 22:24:53 +0800

    v0.9.2 - Day 09.2

--- day09.py
+++ day09.py
@@ -2,6 +2,7 @@
 # John Roy Daradal 
 
 # SolutionA: 27911108
+# SolutionB: 4023754
 
 import itertools 
 from utils import * 
@@ -18,6 +19,22 @@ def day09A():
             print(numbers[i])
             return
 
+def day09B():
+    # full, target = False, 127 
+    full, target = True, 27911108
+
+    _, numbers = input09(full)
+    for i in range(len(numbers)):
+        j = i
+        total = numbers[i]
+        while total < target:
+            j += 1
+            total += numbers[j]
+            if total == target:
+                seq = sorted(numbers[i:j+1])
+                print(seq[0] + seq[-1])
+                return
+
 def hasPairSum(target: int, numbers: list[int]) -> bool:
     for p in itertools.combinations(numbers, 2):
         if sum(p) == target:
@@ -26,3 +43,4 @@ def hasPairSum(target: int, numbers: list[int]) -> bool:
 
 if __name__ == '__main__':
     day09A()
+    day09B()
